2ef42736b14bab4ac2fe4b70bc6cb87bade95e74
Mykel <mykelaballe@gmail.com>
2020-03-18 11:51:25 +0800

    remove house number in personal info security questions

--- src/services/personal_security_questions.js
+++ src/services/personal_security_questions.js
@@ -1,6 +1,5 @@
 export default [
     "What is your date of birth? Please input in mm/dd/yyyy format e.g. 01/31/2020",
-    "What is your house number and barangay?",
     "What month and year did you sign up of ML Wallet? Please input in mm/yyyy format e.g. 01/2020",
     "What is your street name and zip code?"
 ]
