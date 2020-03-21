64c58f6e753a63ae8389a9997d2419f30dfdfddc
Mykel Aballe <mykelaballe@gmail.com>
2020-03-22 00:18:59 +0800

    ios map issue fix 1

--- src/screens/LocatorScreen.js
+++ src/screens/LocatorScreen.js
@@ -75,7 +75,12 @@ class LocatorScreen extends React.Component {
 
 const style = StyleSheet.create({
     map: {
-        flex:1
+        flex:1,
+        left:0,
+        right:0,
+        top:0,
+        bottom:0,
+        position:'absolute'
     },
     marker: {
         width:20,
