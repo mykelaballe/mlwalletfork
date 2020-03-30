d97afeb0161dcdced685eb3e8f3e1f41ca825b83
Mykel Aballe <mykelaballe@gmail.com>
2020-03-30 17:31:46 +0800

    fix

--- src/services/transactional_security_questions.js
+++ src/services/transactional_security_questions.js
@@ -1,5 +1,14 @@
 export default [
-    "When was your last transaction? Please input in mm/dd/yyyy format e.g. 01/31/2020",
-    "How much is your last transaction?",
-    "How much is your account balance?"
+    {
+        text:"When was your last transaction? Please input in mm/dd/yyyy format e.g. 01/31/2020",
+        key:'lasttransdate'
+    },
+    {
+        text:"How much is your last transaction?",
+        key:'lasttransamount'
+    },
+    {
+        text:"How much is your account balance?",
+        key:'balance'
+    }
 ]
