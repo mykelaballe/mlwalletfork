65945d0f9149744ce49e8093301c1b55c8c4c53e
Mykel Aballe <mykelaballe@gmail.com>
2020-04-04 13:59:33 +0800

    fix

--- src/services/endpoints/Bills.js
+++ src/services/endpoints/Bills.js
@@ -119,7 +119,7 @@ export default {
     },
 
     addFavoriteBiller: async payload => {
-        return await Fetch.put(`partners/addToFavorite?walletno=${payload.walletno}&id=${payload.id}`)
+        return await Fetch.put(`partners/addToFavorite`,payload)
     },
 
     removeFavoriteBiller: async payload => {
