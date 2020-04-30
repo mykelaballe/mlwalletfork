a7cd0067cfa18042d64abff7b2e3bd62163a51e1
Mykel Aballe <mykelaballe@gmail.com>
2020-04-30 12:26:14 +0800

    fix

--- src/screens/AddLoadReceiver.js
+++ src/screens/AddLoadReceiver.js
@@ -34,6 +34,8 @@ class Scrn extends React.Component {
             fullname = fullname.trim()
             mobileno = mobileno.trim()
 
+            mobileno = `09${mobileno}`
+
             if(!fullname || !mobileno) Say.some(_('8'))
             else if(!Func.isLettersOnly(fullname)) Say.warn(Consts.error.onlyLetters)
             else if(!Func.isPHMobileNumber(mobileno)) Say.warn(Consts.error.mobile)
