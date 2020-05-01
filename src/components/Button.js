74a6ddb06c10a795623f1c7d8d7242017fb206a8
Mykel Aballe <mykelaballe_prospark@bitbucket.org>
2020-05-02 01:00:54 +0800

    fix

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,63 +4,59 @@ import {Button as Btn} from 'react-native-paper'
 import {Colors, Metrics} from '../themes'
 import {debounce} from 'underscore'
 
-export default props => {
+export default class Button extends React.Component {
 
-    let customStyle = {
-        backgroundColor:Colors.brand
+    handlePress = () => {
+        const {onPress} = this.props
+        debounce(onPress, 500, true)
     }
 
-    let btnStyle = {
-        padding:Metrics.rg
-    }
+    render() {
 
-    let disabledStyle = {}, labelStyle = {}
-
-    if(props.disabled) {
-        disabledStyle.borderWidth = 0
-        disabledStyle.opacity = .6
-        disabledStyle.backgroundColor = Colors.brand
-
-        labelStyle.color = Colors.light
-    }
-
-    //color theme
-    if(props.success) customStyle.backgroundColor = Colors.success
-    else if(props.info) customStyle.backgroundColor = Colors.info
-    else if(props.warning) customStyle.backgroundColor = Colors.warning
-    else if(props.danger) customStyle.backgroundColor = Colors.danger
-    else if(props.dark) customStyle.backgroundColor = Colors.dark
-    else if(props.light) customStyle.backgroundColor = Colors.light
+        let customStyle = {
+            backgroundColor:Colors.brand
+        }
     
-    return (
-        <Btn
-            {...props}
-            style={{
-                ...props.style,
-                borderColor:customStyle.backgroundColor,
-                ...disabledStyle
-            }}
-            labelStyle={{
-                ...labelStyle
-            }}
-            contentStyle={{
-                ...btnStyle
-            }}
-            disabled={props.loading || props.disabled}
-            mode={props.mode || 'contained'}
-            color={customStyle.backgroundColor}
-            onPress={props.onPress}
-            uppercase={false}
-        >
-            {props.t}
-        </Btn>
-    )
-}
-
-const style = StyleSheet.create({
-    container: {
-        paddingVertical:Metrics.rg,
-        paddingHorizontal:Metrics.sm,
-        borderRadius:Metrics.sm
+        let disabledStyle = {}, labelStyle = {}
+    
+        if(props.disabled) {
+            disabledStyle.borderWidth = 0
+            disabledStyle.opacity = .6
+            disabledStyle.backgroundColor = Colors.brand
+    
+            labelStyle.color = Colors.light
+        }
+    
+        //color theme
+        if(props.success) customStyle.backgroundColor = Colors.success
+        else if(props.info) customStyle.backgroundColor = Colors.info
+        else if(props.warning) customStyle.backgroundColor = Colors.warning
+        else if(props.danger) customStyle.backgroundColor = Colors.danger
+        else if(props.dark) customStyle.backgroundColor = Colors.dark
+        else if(props.light) customStyle.backgroundColor = Colors.light
+        
+        return (
+            <Btn
+                {...props}
+                style={{
+                    ...props.style,
+                    borderColor:customStyle.backgroundColor,
+                    ...disabledStyle
+                }}
+                labelStyle={{
+                    ...labelStyle
+                }}
+                contentStyle={{
+                    padding:Metrics.rg
+                }}
+                disabled={props.loading || props.disabled}
+                mode={props.mode || 'contained'}
+                color={customStyle.backgroundColor}
+                onPress={this.handlePress}
+                uppercase={false}
+            >
+                {props.t}
+            </Btn>
+        )
     }
-})
+}
