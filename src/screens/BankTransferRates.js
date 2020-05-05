3fcd9add95b964865b48ff967a869defb57db572
Mykel Aballe <mykelaballe_prospark@bitbucket.org>
2020-05-05 17:22:38 +0800

    fix

--- src/screens/BankTransferRates.js
+++ src/screens/BankTransferRates.js
@@ -24,7 +24,7 @@ export default class Scrn extends React.Component {
     renderItems = ({item}) => (
         <Row style={style.item}>
             <Text center style={{flex:1}}>{item.minAmount} - {item.maxAmount}</Text>
-            <Row style={{flex:2}}>
+            <Row f>
                 <Text center style={{flex:1}}>{item.chargeValue}</Text>
                 <Text center style={{flex:1}}>{item.convenienceValue}</Text>
             </Row>
@@ -39,11 +39,11 @@ export default class Scrn extends React.Component {
            <Screen ns>
                 <Row ar style={{alignItems:'flex-start'}}>
                     <Text center b style={{flex:1}}>Amount</Text>
-                    <View style={{flex:2,alignItems:'center'}}>
+                    <View style={{flex:1,alignItems:'center'}}>
                         <Text center b>Rates</Text>
                         <Row>
-                            <Text center mute style={{flex:1}}>Service Charge</Text>
-                            <Text center mute style={{flex:1}}>Convenience Fee</Text>
+                            <Text center mute style={{flex:1}}>{`Service\nCharge`}</Text>
+                            <Text center mute style={{flex:1}}>{`Convenience\nFee`}</Text>
                         </Row>
                     </View>
                 </Row>
