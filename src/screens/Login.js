f634583df61e41848dbb8bf6487b62e59492f984
Mykel Aballe <mykelaballe_prospark@bitbucket.org>
2020-05-17 17:51:18 +0800

    fix login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -249,7 +249,6 @@ class Scrn extends React.Component {
                             onSubmitEditing={this.handleFocusPassword}
                             autoCapitalize='none'
                             returnKeyType='next'
-                            mask='[0000] [0000] [0000] [00]'
                         />
 
                         <Spacer sm />
