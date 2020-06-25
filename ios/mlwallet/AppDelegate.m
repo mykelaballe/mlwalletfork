c6d729ea849a4818eadc2c1bb37e44d9bf17bfc9
Mykel Aballe <mykel.aballe@redcoresolutions.com>
2020-06-25 10:23:34 +0800

    configure ios face api

--- ios/mlwallet/AppDelegate.m
+++ ios/mlwallet/AppDelegate.m
@@ -12,11 +12,13 @@
 #import <React/RCTRootView.h>
 #import <GoogleMaps/GoogleMaps.h>
 #import <CodePush/CodePush.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   [GMSServices provideAPIKey:@"AIzaSyCxoT1ORnihAKC1FZe5FHYCXA_56CUjfkM"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
