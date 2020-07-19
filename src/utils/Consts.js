5304a3e0c662e0cfc4b82b85fd3ea07ef304c4bf
Mykel Aballe <mykel.aballe@redcoresolutions.com>
2020-07-19 08:57:32 +0800

    setup codepush

--- src/utils/Consts.js
+++ src/utils/Consts.js
@@ -6,8 +6,8 @@ const IS_ANDROID = Platform.OS === 'android'
 const PLAYSTORE_URL = 'https://play.google.com/store/apps/details?id=com.mlhuillier.mlwallet&hl=en'
 const APPSTORE_URL = 'https://apps.apple.com/ph/app/ml-wallet/id962204987'
 
-const CODEPUSH_ANDROID_DEV = 'Kqa8Z6UvgsdJFruYtR3Mq-rE_nu4rtAjy6-ev'
-const CODEPUSH_IOS_DEV = 'yBJeaL3P77cjjC3pZROSybtcceBSmqCXcOKst'
+const CODEPUSH_ANDROID_DEV = '4fqjYGW3KwXH_fdzzIaebsbvfYFBTEchWq_7i'
+const CODEPUSH_IOS_DEV = 'tBS7rLCqUBKL7wu-0atzZIGDXGWFt83_pWuPk'
 
 export default {
 	is_dev:true,
