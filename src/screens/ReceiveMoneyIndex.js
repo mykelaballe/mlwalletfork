6bdfece9c5c8265ea175b7cf0ce664bb4a559b28
Mykel Aballe <mykelaballe@gmail.com>
2020-08-05 19:02:24 +0800

    fixes

--- src/screens/ReceiveMoneyIndex.js
+++ src/screens/ReceiveMoneyIndex.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Screen, Headline, ListMenuItem} from '../components'
+import {Screen, Headline, ListMenuItem, Icon} from '../components'
 import {_, Consts, Say} from '../utils'
+import {Colors} from '../themes'
 
 export default class Scrn extends React.Component {
 
@@ -10,7 +11,7 @@ export default class Scrn extends React.Component {
 
     handlePressDomestic = () => this.props.navigation.navigate('ReceiveMoneyDomestic',{type:Consts.tcn.rmd.code})
 
-    handlePressInternational = () => Say.warn(Consts.error.featureNotAvailable)//this.props.navigation.navigate('ReceiveMoneyInternational',{type:Consts.tcn.rmi.code})
+    handlePressInternational = () => this.props.navigation.navigate('ComingSoon',{title:'Receive Money International',icon:<Icon name='receive_money' style={{width:120,height:120}} />})//Say.warn(Consts.error.featureNotAvailable)//this.props.navigation.navigate('ReceiveMoneyInternational',{type:Consts.tcn.rmi.code})
 
     render() {
 
--- src/screens/SendMoneyIndex.js
+++ src/screens/SendMoneyIndex.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Screen, Headline, ListMenuItem} from '../components'
+import {Screen, Headline, ListMenuItem, Icon} from '../components'
 import {_, Consts, Say} from '../utils'
+import {Colors} from '../themes'
 
 export default class Scrn extends React.Component {
 
@@ -12,7 +13,7 @@ export default class Scrn extends React.Component {
 
     handleSendToKP = () => this.props.navigation.navigate('SendKPIndex',{type:Consts.tcn.skp.code})
 
-    handleSendToBank = () => Say.warn(Consts.error.featureNotAvailable)//this.props.navigation.navigate('SendBankTransferIndex',{type:Consts.tcn.stb.code})
+    handleSendToBank = () => this.props.navigation.navigate('ComingSoon',{title:'Wallet To Bank',icon:<Icon name='send_money' style={{width:120,height:120}} />})//Say.warn(Consts.error.featureNotAvailable)//this.props.navigation.navigate('SendBankTransferIndex',{type:Consts.tcn.stb.code})
 
     render() {
 
