84f9ff9a526f94293858528c39efe2947ca60b52
Mykel Aballe <mykelaballe@gmail.com>
2020-08-31 09:54:44 +0800

    pass deviceID during receive money domestic and international

--- src/services/endpoints/ReceiveMoney.js
+++ src/services/endpoints/ReceiveMoney.js
@@ -1,7 +1,14 @@
 import Fetch from '../../utils/Fetch'
+import Consts from '../../utils/Consts'
 
 export default {
-    receiveMoneyDomestic: async payload => await Fetch.postc('domestic/payout',payload),
+    receiveMoneyDomestic: async payload => await Fetch.postc('domestic/payout',{
+        ...payload,
+        deviceID:Consts.deviceId
+    }),
 
-    receiveMoneyInternational: async payload => await Fetch.post('corporate/payout',payload)
+    receiveMoneyInternational: async payload => await Fetch.post('corporate/payout',{
+        ...payload,
+        deviceID:Consts.deviceId
+    })
 }
