070316c9fff1b953b244e1a72081d71e453640a6
Mykel Aballe <mykelaballe@gmail.com>
2020-09-15 11:18:17 +0800

    dynamic app version in home promo, Home.js hook refactor

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,79 +1,35 @@
-import React from 'react'
-import {View, StyleSheet, InteractionManager, TouchableOpacity, Dimensions, Image, RefreshControl} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {View, StyleSheet, TouchableOpacity, Dimensions, ImageBackground, RefreshControl} from 'react-native'
 import {connect} from 'react-redux'
 import {Creators} from '../actions'
-import {ScrollView, Text, Spacer, FlatList, Ripple, Icon, Balance, Row} from '../components'
+import {ScrollView, Text, Spacer, Ripple, Icon, Balance, Row} from '../components'
 import {Colors, Metrics} from '../themes'
-import {_, Say} from '../utils'
+import {_, Consts} from '../utils'
 import {API} from '../services'
 
 const {width, height} = Dimensions.get('window')
 const ITEM_WIDTH = (width / 4) - (Metrics.lg)
 const ITEM_HEIGHT = 130
 
-class Scrn extends React.Component {
+const ItemUI = ({icon, label, onPress}) => (
+    <Ripple onPress={onPress} style={style.item}>
+        <Icon name={icon} style={style.icon} />
+        <Spacer sm />
+        <Text center sm>{label}</Text>
+    </Ripple>
+)
 
-    static navigationOptions = {
-        headerLeft:<View />,
-        title:'ML Wallet'
-    }
+const Scrn = props => {
 
-    state = {
-        services:[
-            {
-                icon:'add_money',
-                label:'Add\nMoney',
-                onPress:() => this.handleGoToAddMoney()
-            },
-            {
-                icon:'send_money',
-                label:'Send\nMoney',
-                onPress:() => this.handleGoToSendMoney()
-            },
-            {
-                icon:'receive_money',
-                label:'Receive\nMoney',
-                onPress:() => this.handleGoToReceiveMoney()
-            },
-            {
-                icon:'withdraw_cash',
-                label:'Withdraw\bMoney',
-                onPress:() => this.handleGoToWithdrawCash()
-            },
-            {
-                icon:'pay_bills',
-                label:'Pay\nBills',
-                onPress:() => this.handleGoToPayBills()
-            },
-            {
-                icon:'buy_load',
-                label:'Buy\neLoad',
-                onPress:() => this.handleGoToBuyLoad()
-            },
-            {
-                icon:'buy_items',
-                label:'Buy\nItems',
-                onPress:() => this.handleGoToBuyItems()
-            },
-        ],
-        show_balance:true,
-        promo:null,
-        loading:true,
-        refreshing:false
-    }
+    const [refreshing, setRefreshing] = useState(false)
 
-    componentDidMount = () => InteractionManager.runAfterInteractions(this.getData)
-
-    getData = async () => {
-        const {walletno} = this.props.user
+    const getData = async () => {
+        const {walletno} = props.user
 
         try {
-            //let rates = await API.getRates()
-            //this.props.setRates(rates)
-
             let res = await API.getAccountInfo(walletno)
             if(!res.error && res.data) {
-                this.props.updateInfo({
+                props.updateInfo({
                     ...res.data,
                     mobile_no:res.data.mobileNo,
                     email:res.data.EmailAdd
@@ -81,154 +37,88 @@ class Scrn extends React.Component {
             }
         }
         catch(err) {
-            Say.err(err)
+            
         }
 
-        this.setState({
-            refreshing:false
-        })
+        setRefreshing(false)
     }
 
-    handleRefresh = () => this.setState({refreshing:true},this.getData)
-
-    handleGoToAddMoney = () => {
-        const {navigation: {navigate}} = this.props
-        navigate('AddMoneyIndex')
+    const handleRefresh = () => {
+        setRefreshing(true)
+        getData()
     }
 
-    handleGoToSendMoney = () => {
-        const {navigation: {navigate}, hasSeenSendMoneyOnboarding} = this.props
-        if(hasSeenSendMoneyOnboarding) navigate('SendMoneyIndex')
-        else navigate('SendMoneyOnBoarding')
-    }
+    const handleGoToAddMoney = () => props.navigation.navigate('AddMoneyIndex')
 
-    handleGoToReceiveMoney = () => {
-        const {navigation: {navigate}, hasSeenReceiveMoneyOnboarding} = this.props
-        if(hasSeenReceiveMoneyOnboarding) navigate('ReceiveMoneyIndex')
-        else navigate('ReceiveMoneyOnBoarding')
-    }
+    const handleGoToSendMoney = () => props.navigation.navigate(props.hasSeenSendMoneyOnboarding ? 'SendMoneyIndex' : 'SendMoneyOnBoarding')
 
-    handleGoToWithdrawCash = () => {
-        const {navigation: {navigate}, hasSeenWithdrawCashOnboarding} = this.props
-        if(hasSeenWithdrawCashOnboarding) navigate('WithdrawCash')
-        else navigate('WithdrawCashOnBoarding')
-    }
+    const handleGoToReceiveMoney = () => props.navigation.navigate(props.hasSeenReceiveMoneyOnboarding ? 'ReceiveMoneyIndex' : 'ReceiveMoneyOnBoarding')
 
-    handleGoToPayBills = () => {
-        const {navigation: {navigate}, hasSeenPayBillsOnboarding} = this.props
-        if(hasSeenPayBillsOnboarding) navigate('PayBillsIndex')
-        else navigate('PayBillsOnBoarding')
-    }
+    const handleGoToWithdrawCash = () => props.navigation.navigate(props.hasSeenWithdrawCashOnboarding ? 'WithdrawCash' : 'WithdrawCashOnBoarding')
 
-    handleGoToBuyLoad = () => {
-        const {navigation: {navigate}, hasSeenBuyLoadOnboarding} = this.props
-        if(hasSeenBuyLoadOnboarding) navigate('BuyLoadIndex')
-        else navigate('BuyLoadOnBoarding')
-    }
+    const handleGoToPayBills = () => props.navigation.navigate(props.hasSeenPayBillsOnboarding ? 'PayBillsIndex' : 'PayBillsOnBoarding')
 
-    handleGoToBuyItems = () => this.props.navigation.navigate('ComingSoon',{title:'Buy Items',phrase:"We're working on our store.\nStay tuned for updates."})
+    const handleGoToBuyLoad = () => props.navigation.navigate(props.hasSeenBuyLoadOnboarding ? 'BuyLoadIndex' : 'BuyLoadOnBoarding')
 
-    handleViewVerificationLevels = () => this.props.navigation.navigate('VerificationLevels')
+    const handleGoToBuyItems = () => props.navigation.navigate('ComingSoon',{title:'Buy Items',phrase:"We're working on our store.\nStay tuned for updates."})
 
-    renderServices = ({item, index}) => (
-        <Ripple onPress={item.onPress} style={style.item}>
-            <Icon name={item.icon} style={style.icon} />
-            <Spacer sm />
-            <Text center sm>{item.label}</Text>
-        </Ripple>
-    )
+    const   handleViewVerificationLevels = () => props.navigation.navigate('VerificationLevels')
 
-    render() {
+    useEffect(() => {
+        getData()
+    }, [])
 
-        const {status} = this.props.user
-        const {services, refreshing} = this.state
+    return (
+        <>
+            {props.user.status > 0 &&
+            <TouchableOpacity style={style.topBanner} onPress={handleViewVerificationLevels}>
+                <Text center>You are {props.user.status == 1 ? 'semi-verified' : 'fully verified'}. Tap to learn more.</Text>
+            </TouchableOpacity>
+            }
 
-        return (
-            <>
-                {status > 0 &&
-                <TouchableOpacity style={style.topBanner} onPress={this.handleViewVerificationLevels}>
-                    <Text center>You are {status == 1 ? 'semi-verified' : 'fully verified'}. Tap to learn more.</Text>
-                </TouchableOpacity>
-                }
+            <Balance />
 
-                <Balance />
+            <Spacer />
+
+            <ScrollView refreshControl={<RefreshControl colors={[Colors.brand]} refreshing={refreshing} onRefresh={handleRefresh} />}>
+                <View style={{flex:1}}>
+                    <Row ar>
+                        <ItemUI icon='add_money' label={`Add\nMoney`} onPress={handleGoToAddMoney} />
+                        <ItemUI icon='send_money' label={`Send\nMoney`} onPress={handleGoToSendMoney} />
+                        <ItemUI icon='receive_money' label={`Receive\nMoney`} onPress={handleGoToReceiveMoney} />
+                        <ItemUI icon='withdraw_cash' label={`Withdraw\nMoney`} onPress={handleGoToWithdrawCash} />
+                    </Row>
+
+                    <View style={{alignItems:'center'}}>
+                        <Row>
+                            <ItemUI icon='pay_bills' label={`Pay\Bills`} onPress={handleGoToPayBills} />
+
+                            <Spacer h />
+
+                            <ItemUI icon='buy_load' label={`Buy\neLoad`} onPress={handleGoToBuyLoad} />
+
+                            <Spacer h />
+
+                            <ItemUI icon='buy_items' label={`Buy\nItems`} onPress={handleGoToBuyItems} />
+                        </Row>
+                    </View>
+                </View>
 
                 <Spacer />
 
-                {/*<FlatList
-                    data={services}
-                    renderItem={this.renderServices}
-                    numColumns={4}
-                    columnWrapperStyle={{justifyContent:'center'}}
-                />*/}
-                <ScrollView
-                    refreshControl={
-                        <RefreshControl colors={[Colors.brand]} refreshing={refreshing} onRefresh={this.handleRefresh} />
-                    }
-                >
-                    <View style={{flex:1}}>
-                        <Row ar>
-                            <Ripple onPress={this.handleGoToAddMoney} style={style.item}>
-                                <Icon name='add_money' style={style.icon} />
-                                <Spacer sm />
-                                <Text center sm>{`Add\nMoney`}</Text>
-                            </Ripple>
+                <View style={style.footer}>
+                    <ImageBackground source={require('../res/promo_placeholder.png')} style={style.promo} resizeMode='contain'>
+                        <Text light bold style={{marginLeft:Metrics.lg}}>V{Consts.appVersion}</Text>
+                    </ImageBackground>
+                </View>
+            </ScrollView>
+        </>
+    )
+}
 
-                            <Ripple onPress={this.handleGoToSendMoney} style={style.item}>
-                                <Icon name='send_money' style={style.icon} />
-                                <Spacer sm />
-                                <Text center sm>{`Send\nMoney`}</Text>
-                            </Ripple>
-
-                            <Ripple onPress={this.handleGoToReceiveMoney} style={style.item}>
-                                <Icon name='receive_money' style={style.icon} />
-                                <Spacer sm />
-                                <Text center sm>{`Receive\nMoney`}</Text>
-                            </Ripple>
-
-                            <Ripple onPress={this.handleGoToWithdrawCash} style={style.item}>
-                                <Icon name='withdraw_cash' style={style.icon} />
-                                <Spacer sm />
-                                <Text center sm>{`Withdraw\nMoney`}</Text>
-                            </Ripple>
-                        </Row>
-
-                        <View style={{alignItems:'center'}}>
-                            <Row>
-                                <Ripple onPress={this.handleGoToPayBills} style={style.item}>
-                                    <Icon name='pay_bills' style={style.icon} />
-                                    <Spacer sm />
-                                    <Text center sm>{`Pay\nBills`}</Text>
-                                </Ripple>
-
-                                <Spacer h />
-
-                                <Ripple onPress={this.handleGoToBuyLoad} style={style.item}>
-                                    <Icon name='buy_load' style={style.icon} />
-                                    <Spacer sm />
-                                    <Text center sm>{`Buy\neLoad`}</Text>
-                                </Ripple>
-
-                                <Spacer h />
-
-                                <Ripple onPress={this.handleGoToBuyItems} style={style.item}>
-                                    <Icon name='buy_items' style={style.icon} />
-                                    <Spacer sm />
-                                    <Text center sm>{`Buy\nItems`}</Text>
-                                </Ripple>
-                            </Row>
-                        </View>
-                    </View>
-
-                    <Spacer />
-
-                    <View style={style.footer}>
-                        <Image source={require('../res/promo_placeholder.png')} style={style.promo} resizeMode='contain' />
-                    </View>
-                </ScrollView>
-            </>
-        )
-    }
+Scrn.navigationOptions = {
+    headerLeft:<View />,
+    title:'ML Wallet'
 }
 
 const style = StyleSheet.create({
@@ -248,8 +138,6 @@ const style = StyleSheet.create({
         height:40
     },
     footer: {
-        //backgroundColor:'blue',
-        //flex:1,
         justifyContent:'flex-end',
         alignItems:'center',
         height:parseInt(height * .27),
@@ -257,7 +145,8 @@ const style = StyleSheet.create({
     },
     promo: {
         width,
-        height:110
+        height:110,
+        justifyContent:'center'
     }
 })
 
@@ -271,8 +160,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-    updateInfo:newInfo => dispatch(Creators.updateUserInfo(newInfo)),
-    setRates: rates => dispatch(Creators.setKPRates(rates))
+    updateInfo:newInfo => dispatch(Creators.updateUserInfo(newInfo))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Scrn)
